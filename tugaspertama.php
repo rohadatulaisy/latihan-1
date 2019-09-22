f3f95bb796a036cb559633af3ac306c74c2b4b20
rohadatulaisy <55368545+rohadatulaisy@users.noreply.github.com>
2019-09-22 14:29:34 +0700

    Add files via upload

---
+++ tugaspertama.php
@@ -0,0 +1,31 @@
+<?php
+
+print " Hallo, ini tugasnya Rohadatul Aisy yaa :):) <br><br>";
+$s = 10;
+$a = 7;
+$t = 10;
+$m = 5;
+$phi = 3.14;
+$r = 7;
+
+echo "1. Diketahui sisi segitiga sama sisi adalah $s cm dan tingginya $t cm. Tentukan luas dan kelilingnya!<br>";
+echo "Luas = (A x T)/ 2 = ($s x $t)/2 = ";
+echo ($s * $t)/2 ;
+echo " <br> Keliling = S + S + S = $s + $s + $s  = ";
+echo $s + $s + $s ;
+
+
+echo " <br><br> 2. Diketahui alas jajar genjang $a cm, tinggi $t cm, sisi miring $m cm. Tentukan luas dan 
+	kelilignya! <br> ";
+echo "Luas = A x T = $a x $t = " ;
+echo $a * $t ;
+echo "<br> Kelilig = 2 x (alas + miring)= 2x($a+$m) = ";
+echo 2 * ($a + $m);
+
+echo "<br><br> 3. Diketahui jari-jari sebuah lingkaran adalah $r cm. Tentukanlah luas dan kelilignya! <br>";
+echo "Luas = phi x r x r = $phi x $r x$r = ";
+echo $phi * $r *$r ;
+echo "<br> Keliling = 2 x phi x r = 2 x $phi x $r = ";
+echo 2 * $phi *$r ;
+
+?> 
